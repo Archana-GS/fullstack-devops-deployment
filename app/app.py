e0cb0ef52c789cb6a5e6b37798dfb6fa35e2c8cf
Archana-GS <gsarchana93@gmail.com>
2026-05-01 13:25:34 +0530

    refining each code file

--- app/app.py
+++ app/app.py
@@ -19,8 +19,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "App is running successfully!"
+    return "App is running successfully!!!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-    
